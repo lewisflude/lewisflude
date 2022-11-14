8b1470079319b31e71defc5b018b31f0324cf921
Lewis Flude <lewis@lewisflude.com>
2022-11-14 17:10:37 +0000

    Add soehne font to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,40 @@
-import './globals.css'
+import "./globals.css";
+import localFont from "@next/font/local";
+import clsx from "clsx";
+
+const soehneBuch = localFont({
+	src: "./(fonts)/soehne-web-buch.woff2",
+	variable: "--font-soehne-buch",
+});
+const soehneBuchKursiv = localFont({
+	src: "./(fonts)/soehne-web-buch-kursiv.woff2",
+	variable: "--font-soehne-buch-kursiv",
+});
+const soehneFett = localFont({
+	src: "./(fonts)/soehne-web-fett.woff2",
+	variable: "--font-soehne-fett",
+});
+const soehneHalbfett = localFont({
+	src: "./(fonts)/soehne-web-halbfett.woff2",
+	variable: "--font-soehne-halbfett",
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html
+			lang="en"
+			className={`${soehneBuch.variable} ${soehneBuchKursiv.variable} ${soehneFett.variable} ${soehneHalbfett.variable}`}
+		>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<head />
+			<body className="font-soehneBuch">{children}</body>
+		</html>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +9,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        soehneBuch: ["var(--font-soehne-buch)", ...fontFamily.sans],
+        soehneBuchKursiv: [
+          "var(--font-soehne-buch-kursiv)",
+          ...fontFamily.sans,
+        ],
+        soehneFett: ["var(--font-soehne-fett)", ...fontFamily.sans],
+        soehneHalbfett: ["var(--font-soehne-halbfett)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
